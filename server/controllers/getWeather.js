37fafaf6e57156ce5608a2b3ed240701555cfae4
David Gutierrez-McCormack <davidmccormack2000@gmail.com>
2020-03-10 15:35:29 -0400

    Fixed weather API.

---
+++ server/controllers/getWeather.js
@@ -0,0 +1,27 @@
+//import request from 'request';
+const Example = require('../models/examples.server.model.js')
+const fetch = require("node-fetch");
+
+ module.exports = function(req, res,body) {
+    //console.log(req.baseUrl);
+    
+    const search = req.params.city;
+
+
+    async function getWeather() {
+        let response = await fetch('http://api.openweathermap.org/data/2.5/weather?q='+search+'&appid=1889e5a93eae80776956083ea2e9f74d');
+        let data = await response.json();
+        return data;
+          
+    }
+    
+    getWeather().then(data => {
+        if (data.name == null) {
+            res.write("Enter a valid city");
+        }
+        else {
+            res.json({data});
+        }
+        res.end();
+    })
+};
--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('./config/express.js');
 const passport = require("passport");
 const users = require("./routes/users");
-
+const weather = require("./routes/weather");
 // Use env port or default
 const port = process.env.PORT || 5000;
 
@@ -16,5 +16,6 @@ require("./config/passport")(passport);
 
 // Routes
 app.use("/users", users);
+app.use("/weather", weather);
 
 app.listen(port, () => console.log(`Server now running on port ${port}!`));
