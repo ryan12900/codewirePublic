2c7f88b22965cd1132aae821576ee3ad2ee3d28a
David Gutierrez-McCormack <davidmccormack2000@gmail.com>
2020-03-10 20:31:33 -0400

    Fixed error for deployment.

--- server/config/passport.js
+++ server/config/passport.js
@@ -2,7 +2,7 @@ const JwtStrategy = require("passport-jwt").Strategy;
 const ExtractJwt = require("passport-jwt").ExtractJwt;
 const mongoose = require("mongoose");
 const User = mongoose.model("users");
-const keys = require("../config/config.js").secretOrKey;
+const keys = process.env.JWT_SECRET || require("../config/config.js").secretOrKey;
 const opts = {};
 
 opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
