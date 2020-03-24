7ae913e731108e36f463fb7081df675bb1086603
David Gutierrez-McCormack <davidmccormack2000@gmail.com>
2020-03-24 13:53:17 -0400

    Connected register page to back end.

--- client/src/views/Register/index.js
+++ client/src/views/Register/index.js
@@ -3,6 +3,8 @@ import {Link} from "react-router-dom";
 import {Container, Form} from 'semantic-ui-react'
 import { Button, Card } from 'semantic-ui-react'
 import '../main.css'
+import axios from 'axios'
+import serverUrl from '../../assets/server-url';
 
 const styles = {
     container: {
@@ -39,6 +41,40 @@ const styles = {
 };
 
 function Home() {
+    const [form, setForm] = React.useState({
+       firstName: '',
+       lastName: '',
+       email: '',
+       password: '',
+       password2: '',
+        role: '',
+    });
+
+    const handleChange = (e, data) => {  // We use 'data' for semantic ui components.
+        let name, value;
+        if(data){
+            name = data.name;
+            value = data.value;
+        } else {
+            name = e.target.name;
+            value = e.target.value;
+        }
+
+        setForm({...form, [name]: value});
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        axios.post(`${serverUrl}/register`, form)
+            .then(() => {
+                alert("User was successfully created")
+            })
+            .catch(() => {
+                alert("An error occurred while registering!");
+            })
+    };
+
+
     const options = [
         { key: 'a', text: 'Agent', value: 'agent' },
         { key: 'c', text: 'Customer', value: 'customer' },
@@ -55,30 +91,33 @@ function Home() {
                         <Form style={styles.form}>
                             <Form.Field>
                                 <label>First Name</label>
-                                <input required name={'firstName'} placeholder='John' />
+                                <input required name={'firstName'} placeholder='John' onChange={handleChange} value={form.firstName}/>
                             </Form.Field>
                             <Form.Field>
                                 <label>Last Name</label>
-                                <input required name={'lastName'} placeholder='Doe' />
+                                <input required name={'lastName'} placeholder='Doe' onChange={handleChange} value={form.lastName}/>
                             </Form.Field>
                             <Form.Field>
                                 <label>Email</label>
-                                <input required name={'email'} type={'email'} placeholder='jdoe@ufl.edu' />
+                                <input required name={'email'} type={'email'} placeholder='jdoe@ufl.edu' onChange={handleChange} value={form.email}/>
                             </Form.Field>
                             <Form.Field>
                                 <label>Password</label>
-                                <input name={'password'} type={'password'} placeholder='Password' />
+                                <input name={'password'} type={'password'} placeholder='Password' onChange={handleChange} value={form.password}/>
                             </Form.Field>
                             <Form.Field>
                                 <label>Confirm Password</label>
-                                <input name={'password2'} type={'password'} placeholder='Password' />
+                                <input name={'password2'} type={'password'} placeholder='Password' onChange={handleChange} value={form.password2}/>
                             </Form.Field>
                             <Form.Field>
                                 <Form.Select
+                                    name={'role'}
                                     fluid
                                     label='Role'
                                     options={options}
                                     placeholder='Role'
+                                    onChange={handleChange}
+                                    value={form.role}
                                 />
                             </Form.Field>
                         </Form>
@@ -88,9 +127,7 @@ function Home() {
                         <Link to={'/'}>
                             <Button color={'red'} style={styles.button}>Back</Button>
                         </Link>
-                        <Link to={'/dashboard'}>
-                            <Button color={'blue'} style={styles.button}>Sign Up</Button>
-                        </Link>
+                            <Button color={'blue'} style={styles.button} onClick={handleSubmit}>Sign Up</Button>
                     </div>
                 </Card.Content>
             </Card>
