9b9f0fd8bb6f4e101c29d5429c9f1d5379f0ecf5
David Gutierrez-McCormack <davidmccormack2000@gmail.com>
2020-03-31 15:15:40 -0400

    Added a controller to update quiz score.

--- server/controllers/quiz.js
+++ server/controllers/quiz.js
@@ -1,5 +1,15 @@
-const User = ('../models/User');
+const User = require('../models/User');
 
-module.exports = (req,res) =>{
+// Extract user id from params and score from body to update DB.
+module.exports = async (req,res) =>{
+    const {userId} = req.params;
+    const {score} = req.body;
+
+    try {
+        await User.findByIdAndUpdate(userId, {quizScore: score});
+        res.json({data: "Successfully updated quiz score!"})
+    } catch (e) {
+        res.status(500).json({error: "There was an error updating the quiz."})
+    }
 
 };
