84c5162ab2264ead4dda93d735d8bcca8a19a303
gcoto96 <gcoto96@gmail.com>
2020-03-31 00:11:07 -0400

    weather api request working with axios!!!!

--- client/src/views/Dashboard/index.js
+++ client/src/views/Dashboard/index.js
@@ -1,11 +1,40 @@
 import React from 'react';
 import {Link} from "react-router-dom";
 import { Container } from 'semantic-ui-react'
-import { Button, Card } from 'semantic-ui-react'
+import { Button, Card,Input } from 'semantic-ui-react'
 import '../main.css'
 import styles from "../styles";
+import axios from 'axios';
+import serverURL from '../../assets/server-url';
 
 function Dashboard() {
+
+    const [weather,setweather] = React.useState({
+        city:'',
+    });
+
+    //Handler
+    const handlechange= (e) => {
+        const weather_ = e.target.value;
+        setweather(weather_);
+    };
+
+    const handleSubmit = (e) =>{
+      e.preventDefault();
+
+      axios.get(`${serverURL}/weather/`+weather)
+          .then(response => {
+              alert("Working");
+              console.log(weather);
+              console.log(response.data);
+          })
+          .catch(err =>{
+              console.log(weather);
+              console.log(err);
+          })
+    };
+
+
     return (
         <Container style={styles.container}>
             <Card style={styles.card}>
@@ -15,8 +44,12 @@ function Dashboard() {
                     </Card.Header>
                     <Card.Description >
                         <h2>Weather Information</h2>
-                        <p>Loading...</p>
+                        <label> City </label>
+                            <Input type="name" placeholder="City" value={weather.city}  onChange={handlechange} />
                     </Card.Description>
+                    <div>
+                    <Button color={'blue'} style={styles.button} onClick={handleSubmit} style={styles.button}>Submit</Button>
+                    </div>
                 </Card.Content>
             </Card>
         </Container>
