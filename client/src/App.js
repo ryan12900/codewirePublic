1c9ed76e46ec51d937d7019bd4306eb86e3f8d21
David Gutierrez-McCormack <davidmccormack2000@gmail.com>
2020-03-31 21:58:40 -0400

    Made all front end routes protected.

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Route, Switch, Redirect  } from 'react-router-dom';
+import ProtectedRoute from './components/ProtectedRoute';
 import Home from "./views/Home/Home";
 import Register from './views/Register';
 import LogIn from './views/LogIn'
@@ -13,23 +14,28 @@ import Client_info from "./views/Admin/Client_Info";
 import Info_accident from "./views/Admin/Info_accident";
 import Statistic_city from "./views/Admin/Statistic_city";
 
+
 const App = () => {
+
+    const [auth, setAuth] = React.useState({});
+
+
   return (
     <div>
         {/* <NavBar />*/}
         <img src={Background_pic} class='backgroundStyles'/>
         <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/register" component={Register} />
-        <Route exact path="/log-in" component={LogIn} />
-        <Route exact path="/dashboard" component={Dashboard} />
-        <Route exact path="/quiz" component={Quiz} />
-        <Route exact path="/admin" component={Admin} />
-        <Route exact path="/add_accident" component={AddAccident} />
-        <Route exact path="/client_info" component={Client_info} />
-        <Route exact path="/info_accident" component={Info_accident}/>
-        <Route exact path="/statistic_city" component={Statistic_city} />
-        <Redirect to={'/'}/>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/log-in" render={() => <LogIn setAuth={setAuth}/>} />
+            <ProtectedRoute auth={auth} exact access={'customer'} path="/dashboard" component={Dashboard} />
+            <ProtectedRoute auth={auth} exact access={'customer'} path="/quiz" component={Quiz} />
+            <ProtectedRoute auth={auth} exact access={'agent'} path="/admin" component={Admin} />
+            <ProtectedRoute auth={auth} exact access={'agent'} path="/add_accident" component={AddAccident} />
+            <ProtectedRoute auth={auth} exact access={'agent'} path="/client_info" component={Client_info} />
+            <ProtectedRoute auth={auth} exact access={'agent'} path="/info_accident" component={Info_accident}/>
+            <ProtectedRoute auth={auth} exact access={'customer'} path="/statistic_city" component={Statistic_city} />
+            <Redirect to={'/'}/>
       </Switch>
     </div>
   );
