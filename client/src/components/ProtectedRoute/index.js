f234837bd6f7d19c600f6a0aca91e8c7d2044838
David Gutierrez-McCormack <davidmccormack2000@gmail.com>
2020-03-31 21:11:28 -0400

    Created protected route component.

---
+++ client/src/components/ProtectedRoute/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Route, Redirect  } from 'react-router-dom';
+
+
+export default (props) => {
+    // Extract auth and access from role.
+    const {auth, access, ...other} = props;
+
+    // If we are not logged in, redirect to home page.
+    if(!auth){
+        return <Redirect to={'/'}/>
+    }
+
+    // If the auth role and access don't match, redirect to according home page.
+    if(auth.role !== access){
+        if(access === 'customer'){
+            return <Redirect to={'/dashboard'}/>
+        } else {
+            return <Redirect to={'/admin'}/>
+        }
+    }
+
+    // Otherwise, return a route to the destination.
+    return <Route {...other}/>
+
+};
