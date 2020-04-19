e21ac157078f9966450bc7e1491279c756f01217
John Hook <jhook1@ufl.edu>
2020-04-18 22:28:55 -0400

    Backend accident API update for cities

--- server/controllers/accident.controller.js
+++ server/controllers/accident.controller.js
@@ -5,25 +5,55 @@ const stateIds = ['alabama','alaska','','arizona','arkansas','california','','co
 
 function getIdList(req){
     let stateEntry = req.params.stateName;
-    //console.log('State: ',stateEntry);
     stateEntry = stateEntry.toLowerCase();
-    //console.log('State (small): ',stateEntry);
-    //res.write(stateEntry);
     let stateList = stateEntry.split(',');
-    //console.log('stateList: ', stateList);
     let idList=[];
     stateList.forEach(state=>{
-        //res.write(state);
         function getIndex(checkState) {
             return state === checkState;
         }
         let id = stateIds.findIndex(getIndex) +1;
         idList.push(id);
-        //idList.push(id+',');
-        //res.write(id.toString());
     });
     return idList;
-    //console.log('idList: ',idList);
+}
+
+exports.byStateCityYear=(req,res)=>{
+    getAccidents(getIdList(req).toString()).then((data)=>{
+        res.write(JSON.stringify(data));
+        res.end();
+    })
+
+    async function getAccidents(IDs){
+        console.log('year=',req.params.year);
+
+        let search = true;
+        let response;
+        let i = 1;
+        let data;
+        let cityNameUp = req.params.cityName.toUpperCase();
+        console.log('city name=',cityNameUp);
+        while(search){
+            response = await fetch('https://crashviewer.nhtsa.dot.gov/CrashAPI/crashes/GetCrashesByLocation?fromCaseYear='+req.params.year+'&toCaseYear='+req.params.year+'&state='+IDs+'&county='+i+'&format=json')
+                .catch(err=>console.log(err));
+            data = await response.json();
+
+            if (data.Results[0].length>0){
+                console.log('county:', data.Results[0][0].COUNTYNAME);
+
+                for(let accident of data.Results[0]){
+                    if (accident.CITYNAME === cityNameUp){
+                        search = false;
+                        break;
+                    }
+                }
+            }
+
+            i++;
+        }
+
+        return data.Results[0].filter(accident=>accident.CITYNAME===cityNameUp);
+    }
 }
 
 exports.analytics=(req,res)=>{
@@ -33,18 +63,12 @@ exports.analytics=(req,res)=>{
     });
 
     async function getAccidents(IDs){
-        //console.log(IDs);
-        //console.log('County: ',req.params.county);
         let years = req.params.year.split(',');
         if (years.length === 1){
             years.push(years[0]);
         }
-        //console.log('froYear: ',years[0]);
-        //console.log('toYear',years[1]);
         let response = await fetch('https://crashviewer.nhtsa.dot.gov/CrashAPI/analytics/GetInjurySeverityCounts?fromCaseYear='+years[0]+'&toCaseYear='+years[1]+'&state='+IDs+'&format=json');
-        //console.log('response: ', response);
         let data = await response.json();
-        //console.log('first data: ', data);
         return data;
     }
 }
@@ -56,85 +80,55 @@ exports.byStateCountyYear=(req,res)=>{
     });
 
     async function getAccidents(IDs){
-        //console.log(IDs);
-        //console.log('County: ',req.params.county);
         let years = req.params.year.split(',');
         if (years.length === 1){
             years.push(years[0]);
         }
-        //console.log('froYear: ',years[0]);
-        //console.log('toYear',years[1]);
         let response = await fetch('https://crashviewer.nhtsa.dot.gov/CrashAPI/crashes/GetCrashesByLocation?fromCaseYear='+years[0]+'&toCaseYear='+years[1]+'&state='+IDs+'&county='+req.params.county+'&format=json');
-        //console.log('response: ', response);
         let data = await response.json();
-        //console.log('first data: ', data);
         return data;
     }
 }
 
 exports.byStateYear= (req,res)=>{
-    //let idList=getIdList(req);
     getAccidents(getIdList(req).toString()).then((data)=>{
         res.write(JSON.stringify(data.Results));
         res.end();
     });
 
     async function getAccidents(IDs){
-        //console.log(IDs);
         let years = req.params.year.split(',');
         if (years.length === 1){
             years.push(years[0]);
         }
-        //console.log('froYear: ',years[0]);
-        //console.log('toYear',years[1]);
         let response = await fetch('https://crashviewer.nhtsa.dot.gov/CrashAPI/crashes/GetCaseList?states='+IDs+'&fromYear='+years[0]+'&toYear='+years[1]+'&minNumOfVehicles=1&maxNumOfVehicles=100&format=json');
-        //console.log('response: ', response);
         let data = await response.json();
-        //console.log('first data: ', data);
         return data;
     }
 };
 
 exports.byState = (req,res)=>{
-    //let idList=getIdList(req);
     getAccidents(getIdList(req).toString()).then((data)=>{
-        //res.write('hit');
-
-        //console.log('Results: ', data.Results);
         res.write(JSON.stringify(data.Results));
-
-        //data.Results.forEach(result=>res.send(result));
         res.end();
     });
 
     async function getAccidents(IDs){
-        //console.log(IDs);
         let response = await fetch('https://crashviewer.nhtsa.dot.gov/CrashAPI/crashes/GetCaseList?states='+IDs+'&fromYear=2010&toYear=2020&minNumOfVehicles=1&maxNumOfVehicles=100&format=json');
-        //console.log('response: ', response);
         let data = await response.json();
-        //console.log('first data: ', data);
         return data;
     }
-
-    //res.end();
 };
 
 exports.accidents = function(req, res) {
     async function getAccidents(){
         let response = await fetch('https://crashviewer.nhtsa.dot.gov/CrashAPI/crashes/GetCaseList?states=1,51&fromYear=2010&toYear=2020&minNumOfVehicles=1&maxNumOfVehicles=100&format=json');
-        //console.log('response: ', response);
         let data = await response.json();
-        //console.log('first data: ', data);
         return data;
     }
 
     getAccidents().then((data)=>{
-        //res.write('hit');
-
-        //console.log('Results: ', data.Results);
         res.write(JSON.stringify(data.Results));
-
-        //data.Results.forEach(result=>res.send(result));
         res.end();
     })
 };
--- server/routes/accident.js
+++ server/routes/accident.js
@@ -6,6 +6,7 @@ const accident = require('../controllers/accident.controller.js'),
 router.route('/states=:stateName').get(accident.byState);
 router.route('/states=:stateName/years=:year').get(accident.byStateYear);
 router.route('/state=:stateName/county=:county/years=:year').get(accident.byStateCountyYear);
+router.route('/state=:stateName/city=:cityName/year=:year').get(accident.byStateCityYear);
 router.route('/analytics/state=:stateName/years=:year').get(accident.analytics);
 
 module.exports = router;
