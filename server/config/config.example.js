666873745d99ff456e463166831c88de83a7938d
John Hook <jhook1@ufl.edu>
2020-04-20 20:40:17 -0400

    Remove config.example.js

--- server/config/config.example.js
+++
@@ -1,9 +0,0 @@
-//This file holds any configuration variables we may need
-//'config.js' is usually ignored by git to protect sensitive information, such as your database's username and password
-
-module.exports = {
-    db: {
-        uri: 'mongodb+srv://jhook1:%230805166Jh%21@cluster0-fgscp.mongodb.net/test?retryWrites=true&w=majority', //place the URI of your mongo database here.
-        secretOrKey: 'secret'
-    }
-};
